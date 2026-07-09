7172f4b4cc9a48b6f82e771b43f82bbedefa4796
Nana Baah <hellonanab@outlook.com>
2026-07-09 13:32:11 +0100

    Update website

---
+++ Index.hmtl/styles/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("Thanks for your interest! Please fill in the contact form to book a session.");
+}
+
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Thanks for your interest! Please complete the contact form below to book your session.";
+}
